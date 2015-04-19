d7ad70569db39af57250bd091ff7865ab0969f4c
keke <keke@KMP.local>
2015-04-19 22:23:13 +0800

    update ISBN extractor handler

---
+++ back/services/extractor/src/main/java/net/luerything/extractor/ISBNExtractorHandler.java
@@ -0,0 +1,72 @@
+package net.luerything.extractor;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.sax.ContentHandlerDecorator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author keke
+ */
+public class ISBNExtractorHandler extends ContentHandlerDecorator {
+  private static final Logger LOG = LoggerFactory.getLogger(ISBNExtractorHandler.class);
+  private static final Pattern ISBN = Pattern.compile("(isbn|International Standard Book Number-13)(\\D)*([\\d-]+)", Pattern.CASE_INSENSITIVE);
+  private static final String ISBN_ID = "dc:identifier";
+  private static final int CONTEXT_LENGTH = 100;
+  private final Metadata metadata;
+  private final StringBuilder stringBuilder;
+
+  public ISBNExtractorHandler(ContentHandler handler, Metadata metadata) {
+    super(handler);
+    this.metadata = metadata;
+    this.stringBuilder = new StringBuilder();
+  }
+
+  @Override
+  public void characters(char[] ch, int start, int length) throws SAXException {
+    try {
+      final String text = new String(Arrays.copyOfRange(ch, start, start + length));
+      stringBuilder.append(text);
+      super.characters(ch, start, length);
+    } catch (SAXException e) {
+      handleException(e);
+    }
+  }
+
+  @Override
+  public void endDocument() throws SAXException {
+    super.endDocument();
+    Matcher m = ISBN.matcher(stringBuilder);
+    int totalLength = stringBuilder.length();
+    while (m.find()) {
+      if (LOG.isDebugEnabled()) {
+        int start = m.start() - CONTEXT_LENGTH;
+        if (start < 0) start = 0;
+        int end = m.end() + CONTEXT_LENGTH;
+        if (end > totalLength) {
+          end = totalLength;
+        }
+        char[] buf = new char[end - start];
+        stringBuilder.getChars(start, end, buf, 0);
+        LOG.debug("Found ISBN {}, Context:\r\n", m.group(3), new StringBuffer().append(buf).toString());
+      }
+
+      metadata.add(ISBN_ID, "urn:isbn:" + m.group(3));
+    }
+  }
+
+  public ISBNExtractorHandler() {
+    this(new DefaultHandler(), new Metadata());
+  }
+
+  public ISBNExtractorHandler(Metadata metadata) {
+    this(new DefaultHandler(), metadata);
+  }
+}
--- back/services/extractor/src/test/java/net/luerything/extractor/ExtractorTestMain.java
+++ back/services/extractor/src/test/java/net/luerything/extractor/ExtractorTestMain.java
@@ -3,38 +3,72 @@ package net.luerything.extractor;
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.TeeContentHandler;
+import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.Reader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author keke
  */
 public class ExtractorTestMain {
+  private static final Set<String> SUPPORT_TYPES = new HashSet<>(Arrays.asList("pdf", "epub+zip"));
 
-  public static void main(String[] args) throws IOException, TikaException {
-//    TikaConfig config = TikaConfig.getDefaultConfig();
-    File file = new File("./src/test/resources/local_data/test.pdf");
+  public static void main(String[] args) throws IOException, TikaException, SAXException {
+    String root = "/Users/keke/Calibre Library";
+    AutoDetectParser parser = new AutoDetectParser();
     Tika tika = new Tika();
-//    InputStream is = ExtractorMain.class.getResourceAsStream("./src/test/resources/local_data/test.pdf");
+    Files.walkFileTree(FileSystems.getDefault().getPath(root), EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+//        System.out.println("Visit path : " + file);
+        if (attrs.isRegularFile() && !file.toFile().isHidden()) {
+          try {
+//            String type = tika.detect(file.toFile());
+
+            MediaType type = MediaType.parse(tika.detect(file.toFile()));
+            if (type.getType().equals("application") && SUPPORT_TYPES.contains(type.getSubtype())) {
+
+              Metadata metadata = new Metadata();
+              parser.parse(new FileInputStream(file.toFile()), new TeeContentHandler(new ISBNExtractorHandler(metadata)), metadata, new ParseContext());
+//    System.out.println("Content is " + type);
+              if (metadata.getValues("dc:identifier").length == 0) {
+                System.out.println("Visit path : " + file);
+                System.out.println("Metadata is " + metadata);
+              }
+            }
+          } catch (IOException e) {
+            System.out.println("Can not access: " + file);
+          } catch (SAXException e) {
+            e.printStackTrace();
+          } catch (TikaException e) {
+            e.printStackTrace();
+          }
+        }
+        return FileVisitResult.CONTINUE;
+      }
+    });
+//    File file = new File("./src/test/resources/local_data/test.pdf");
+    File file = new File("/Users/keke/Calibre Library/Unknown/Springer.Social Group Utility Maximization.2015 (1754)/Springer.Social Group Utility Maximization - Unknown.pdf");
 
 
     Metadata metadata = new Metadata();
 
-    Reader reaer = tika.parse(new FileInputStream(file), metadata);
+    parser.parse(new FileInputStream(file), new TeeContentHandler(new ISBNExtractorHandler(metadata)), metadata, new ParseContext());
 //    System.out.println("Content is " + type);
     System.out.println("Metadata is " + metadata);
 
-//    PDDocument doc = PDDocument.load(file);
-//    PDDocumentInformation info = doc.getDocumentInformation();
-//    Set<String> keys = info.getMetadataKeys();
-//    for(String key : keys){
-//      System.out.println(key+"= " + info.getCustomMetadataValue(key));
-//    }
-//    PDMetadata md = doc.getDocumentCatalog().getMetadata();
-//
-//    System.out.println(md.get);
+
   }
 }
